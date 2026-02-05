d571b1f3f9be42f06b773bb80149350044f128cc
Aistis Verikas <chewingstraws@gmail.com>
2026-02-05 20:26:19 +0200

    Add Struct, variables, libraries

---
+++ v01.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// Var. 2; Visi duomenu rinkiniai su std::vector
+
+using namespace std;
+
+struct Studentas {
+    string vardas, pav;
+    vector<int> paz;
+    int egzaminas; 
+    double galutinis; //Galutinis rezultatas
+};
+
+int main()
+{
+    Studentas A;
+    int n; //Pazymiu skaicius
+    int suma = 0; //Pazymiu suma (Vidurkiui apskaiciuoti)
+    int choiceRez = 0; //Galutinio rez. isvedimo pasirinkimas
+    int choiceEnd = 0; //Programos baigties pasirinkimas;
+    double vidurkis = 0, mediana = 0;
+
+
+    return 0;
+}
