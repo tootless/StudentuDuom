13c0a7126195c8e554e163e306e23a5f905038da
tootless <chewingstraws@gmail.com>
2026-02-28 01:48:43 +0200

    Implemented result sorting logic
    Lambda expression with choice capture clause

--- Program files/Vector_VS/functions.h
+++ Program files/Vector_VS/functions.h
@@ -1,8 +1,28 @@
-﻿//Global functions declarations
+//Studentas struct function declarations
 #pragma once
 
 #include <string>
-#include <fstream>
+#include <vector>
+
+struct Studentas {
+	std::string vardas, pav;
+    std::vector<int> paz;
+    int egzaminas = 0;
+    double galutinisVid = 0;
+    double galutinisMed = 0;
+
+    //Darbas su failais
+    static std::vector<Studentas> read_file(const std::string filename, int& suma);
+    static void write_file(const std::string filename, const std::vector<Studentas>& Studentai);
+
+    //Darbas su ekranu
+    void varpav_input();
+    void paz_input(int& suma);
+    void egz_input();
+    void rand_paz(int pazSk, int& suma);
+    void rand_egz();
+    void rand_stud();
+};
 
 void menu();
 void input_validation(int& choice, int lowEnd, int highEnd, std::string optionalPrompt = "");
